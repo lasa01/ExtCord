58102347711308b74486d7694a37d07a4b61910c
Lassi Säike <lassisaike11@gmail.com>
2020-01-18 16:46:41 +0200

    Updated package index

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,14 @@ export { Argument } from "./commands/arguments/Argument";
 export { StringArgument } from "./commands/arguments/StringArgument";
 export { IntArgument } from "./commands/arguments/IntArgument";
 export { FloatArgument } from "./commands/arguments/FloatArgument";
+export { CommandArgument } from "./commands/arguments/CommandArgument";
+export { MemberArgument } from "./commands/arguments/MemberArgument";
+export { RoleArgument } from "./commands/arguments/RoleArgument";
+export { TextChannelArgument } from "./commands/arguments/TextChannelArgument";
+export { UserArgument } from "./commands/arguments/UserArgument";
 export { BuiltInArguments } from "./commands/arguments/BuiltinArguments";
 export { CommandPhrases } from "./commands/CommandPhrases";
+export { GuildAliasEntity } from "./commands/database/GuildAliasEntity";
 
 export { Config } from "./config/Config";
 export { ConfigEntry } from "./config/entry/ConfigEntry";
@@ -43,6 +49,7 @@ export { PhraseGroup } from "./language/phrase/PhraseGroup";
 export { TemplatePhrase } from "./language/phrase/TemplatePhrase";
 export { MessagePhrase } from "./language/phrase/MessagePhrase";
 export { DynamicFieldMessagePhrase } from "./language/phrase/DynamicFieldMessagePhrase";
+export { ListPhrase } from "./language/phrase/ListPhrase";
 
 export { Modules } from "./modules/Modules";
 export { Module } from "./modules/Module";
@@ -52,6 +59,14 @@ export { Permission } from "./permissions/Permission";
 export { PermissionGroup } from "./permissions/PermissionGroup";
 export { MemberPermissionEntity } from "./permissions/database/MemberPermissionEntity";
 export { RolePermissionEntity } from "./permissions/database/RolePermissionEntity";
+export { PermissionPrivilege } from "./permissions/PermissionPrivilege";
+export { CustomPrivilege } from "./permissions/CustomPrivilege";
+export { MemberPrivilegeEntity } from "./permissions/database/MemberPrivilegeEntity";
+export { RolePrivilegeEntity } from "./permissions/database/RolePrivilegeEntity";
+export { CustomPrivilegeEntity } from "./permissions/database/CustomPrivilegeEntity";
+export { CustomPrivilegeIncludeEntity } from "./permissions/database/CustomPrivilegeIncludeEntity";
+export { CustomPrivilegePermissionEntity } from "./permissions/database/CustomPrivilegePermissionEntity";
 
 export { Serializer } from "./util/Serializer";
 export { Logger } from "./util/Logger";
+export { IExtendedGuild, IExtendedMember, IExtendedMessage, IExtendedRole, IExtendedUser } from "./util/Types";
